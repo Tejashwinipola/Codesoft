dbb6679c38b8f91e028b0b3bee43b0dbeab478f0
Tejashwinipola <149751689+Tejashwinipola@users.noreply.github.com>
2025-04-08 17:32:57 +0530

    Update StdentGradeCalculator.java

--- StdentGradeCalculator.java
+++ StdentGradeCalculator.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-
-public class MarksCalculator {
-    public static void main(String[] args) {
+public class Studentgradecalculator {
+	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of subjects: ");
         int numSubjects = scanner.nextInt();
@@ -34,4 +33,5 @@ public class MarksCalculator {
 
         scanner.close();
     }
+
 }
